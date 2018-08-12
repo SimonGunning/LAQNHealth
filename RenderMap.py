f414713f03cf97ddf3f31a41ee24a61078de540b
Simon Gunning <simonmgunning@gmail.com>
2018-08-12 20:34:30 +0100

    new v of rendermap

--- RenderMap.py
+++ RenderMap.py
@@ -6,23 +6,29 @@ Created on Sat Mar  3 18:24:34 2018
 """
 import geopandas as gpd
 import pandas as pd
+from shapely.geometry import Point
 import numpy as np
 from matplotlib import pyplot as plt
 import pickle
-with open('parrot1.pickle', 'rb') as handle:
-    MonitorsDF = pickle.load(handle)
+#with open('parrot1.pickle', 'rb') as handle:
+#    MonitorsDF = pickle.load(handle)
+MonitorsDF = pd.read_csv('C:\\Users\\gunning\\Documents\\Anatest\\monlocsFinal.csv')
+#MonitorsDF['geometry'] = MonitorsDF.apply(lambda z: Point(z.X, z.Y), axis=1)
+MonitorsDF = gpd.GeoDataFrame(MonitorsDF)
 
 wardDF = gpd.read_file('C:\\Users\\gunning\\Documents\\simon\\MSCProject\\\London-wards-2014\\London-wards-2014 (1)\\London-wards-2014_ESRI\\London_Ward.shp')
 
-wardDF = pd.merge(wardDF, MonitorsDF, left_on='NAME', right_on='Ward')
+MonitorsDF = pd.merge(wardDF, MonitorsDF, left_on='NAME', right_on='Ward', how='inner')
 
 print(wardDF.head(5))
 
 #sLength = len(wardDF['NAME'])
 #wardDF['e'] = pd.Series(np.random.randn(sLength), index=wardDF.index)
 #print(ccg.head(5))
-wardDF.plot(column='PM10')
+base = wardDF.plot(color='white', edgecolor='black')
 plt.title("Ward and NO2 map")
+
+MonitorsDF.plot(ax=base,column='PM10')
 
 #plt.xlabel('position')
 #plt.ylabel('value')
