ece66eee182513165c1d97e3348d0dca5dfdb716
Simon Gunning <simonmgunning@gmail.com>
2018-08-22 21:05:41 +0100

    Add files via upload

--- getMonitorWards.py
+++ getMonitorWards.py
@@ -6,11 +6,15 @@ import requests
 import json
 import pandas as pd
 import pickle
-df = pd.DataFrame(columns=['Ward','Monitor','Longitude','Latitude','NO2','PM10','DUST'])
-#df = {}
+import time
+print("Enter the year that you wish to get pollution monitor data for.")
+year = input("Year ")
 
+RequestString = "http://api.erg.kcl.ac.uk/AirQuality/Annual/MonitoringObjective/GroupName=LAQN/Year=" + year + "/Json"
+df = pd.DataFrame(columns=['Year','Ward','Monitor','Longitude','Latitude','NO2','PM10','DUST'])
+#r = requests.get('http://api.erg.kcl.ac.uk/AirQuality/Annual/MonitoringObjective/GroupName=LAQN/Year=2018/Json')
+r = requests.get(RequestString)
 
-r = requests.get('http://api.erg.kcl.ac.uk/AirQuality/Annual/MonitoringObjective/GroupName=LAQN/Year=2018/Json')
 print(r.status_code)
 r.json()
 count = 0
@@ -24,7 +28,7 @@ for Site in monitors["SiteObjectives"]["Site"]:
     dust = ""
     myNO2 = ""
     myPM10 = ""
-
+    res = ""
     mySiteCode = Site["@SiteCode"]
     myObjectives = Site["Objective"]
     for obs in myObjectives:
@@ -37,22 +41,36 @@ for Site in monitors["SiteObjectives"]["Site"]:
                 myPM10 = obs["@Value"]
 
     wardLookUpq = "http://api.postcodes.io/postcodes?lon=" + Site["@Longitude"] + "&lat=" + Site["@Latitude"]
+#    time.sleep(3)
 #    print(wardLookUpq)
     s = requests.get(wardLookUpq)
-#    print(s.status_code)
     s.json()
     locations = json.loads(s.text)
+#    if locations["result"][0]["admin_ward"] is None:
+#        print("loc res 1 = " + locations["result"][1]["admin_ward"])
     try:
         res = locations["result"][0]["admin_ward"]
-        df.loc[res] = [res,mySiteCode, Site['@Longitude'], Site['@Latitude'], myNO2, myPM10, dust]
+        df.loc[res] = [year, res,mySiteCode, Site['@Longitude'], Site['@Latitude'], myNO2, myPM10, dust]
 
     except TypeError:
-        print("no ward data")
-        print(mySiteCode)
+        NoLoc = "NoLoc" + str(count)
+        df.loc[NoLoc] = [year, NoLoc,mySiteCode, Site['@Longitude'], Site['@Latitude'], myNO2, myPM10, dust]
+
+        print("no ward data " + mySiteCode)
+        print("res " + res)
+#        print("loc res conty = " + locations["result"][0]["admin_county"])
+        print("long " + Site['@Longitude'])
+        print("lat " + Site['@Latitude'])
+        print("myNO2 " + myNO2)
+        print("myPM10 " + myPM10)
+        print("dust " + dust)
+        print(s.status_code)
+
     count = count +1
-print(df)
-with open('parrot1.pickle', 'wb') as handle:
-    pickle.dump(df, handle, protocol=pickle.HIGHEST_PROTOCOL)
+#print(df)
+df.to_csv("monlocs")
+#with open('parrot1.pickle', 'wb') as handle:
+#    pickle.dump(df, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 #df.to_pickle("./parrot.pkl")
 print(count)
