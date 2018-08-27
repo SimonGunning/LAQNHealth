097e9924be565ef6310c84d7a7f55653672eaa08
Simon Gunning <simonmgunning@gmail.com>
2018-08-27 20:49:19 +0100

    Add files via upload

--- LinRegression.py
+++ LinRegression.py
@@ -1,49 +1,51 @@
 #Read the superDFs concatenate them and perform regressions
 # This requires the latest version of Pandas
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
+
 from sklearn import datasets, linear_model
 from sklearn.metrics import mean_squared_error, r2_score
-
-
 import pandas as pd
 from tkinter import *
+from tkinter.filedialog import askopenfilename
 import tkinter.filedialog
 print("Select the files to analyse")
+Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
 
-root = Tk()
-filez = tkinter.filedialog.askopenfilenames(parent=root,title='Choose a file')
-fileList = list(root.tk.splitlist(filez))
-frames =[]
-for i in range (0 , len(fileList)):
-    ADF = pd.read_csv(fileList[i])
-    frames.append(ADF)
-    ADF.iloc[0:0]
-result = pd.concat(frames, ignore_index=True)
-result.to_csv("thebus.csv")
+filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+print(filename)
 
-# # Load the diabetes dataset
-# diabetes = datasets.load_diabetes()
-# # Use only one feature
-# diabetes_X = diabetes.data[:, np.newaxis, 2]
-# # Split the data into training/testing sets
-# diabetes_X_train = diabetes_X[:-20]
-# diabetes_X_test = diabetes_X[-20:]
-# # Split the targets into training/testing sets
-# diabetes_y_train = diabetes.target[:-20]
-# diabetes_y_test = diabetes.target[-20:]
+result = pd.read_csv(filename)
+result = result.dropna(subset=['NO2'])
 
+#prevNO2 = pd.DataFrame([result["Prevalence_perc"],result["NO2"]])
+print(result)
+x_train = result["Prevalence_perc"].values.reshape(-1, 1)
+y_train= result["NO2"].values.reshape(-1, 1)
 # Create linear regression object
 regr = linear_model.LinearRegression()
 
 # Train the model using the training sets
-regr.fit(result["Prevalence_perc"], result["NO2"])
+regr.fit(x_train, y_train)
+print("Rsqared = " + str(regr.score(x_train,y_train)))
+print("Intercept = " + str(regr.intercept_))
 
 # Make predictions using the testing set
 #diabetes_y_pred = regr.predict(diabetes_X_test)
 
 # The coefficients
 print('Coefficients: \n', regr.coef_)
+colors = (0,0,0)
+area = np.pi*3
+
+plt.scatter(x_train, y_train, s=area, c=colors, alpha=0.5)
+plt.title('Scatter plot pythonspot.com')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
+
+
+
 # The mean squared error
 #print("Mean squared error: %.2f"
 #      % mean_squared_error(diabetes_y_test, diabetes_y_pred))
