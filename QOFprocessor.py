cfada368964501b55c4bbe24a97ae855b5c71292
Simon Gunning <simonmgunning@gmail.com>
2018-09-02 19:27:28 +0100

    Add files via upload

--- QOFprocessor.py
+++ QOFprocessor.py
@@ -5,38 +5,36 @@ import requests
 import json
 from tkinter  import Tk
 from tkinter.filedialog import askopenfilename
-QOFMetaData = [[2013,8,9],[2014,14,15],[2015,12,13],[2016,13,14],[2017,16,17]]
-QOFMetaDatadf = pd.DataFrame(QOFMetaData,columns=['Year','ListSizePosition','RegisterPosition'])
+QOFMetaData = [[2013,0,6,8,9],[2014,0,12,14,15],[2015,0,9,12,13],[2016,0,11,13,14],[2017,0,11,16,17]]
+QOFMetaDatadf = pd.DataFrame(QOFMetaData,columns=['Year','RegionPos','PracticeCodePos','ListSizePos','RegisterPos'])
 QOFMetaDatadf = QOFMetaDatadf.set_index('Year')
 print(QOFMetaDatadf)
-
-
 print("Enter the year of the QOF")
 year = input("Year =")
-print(QOFMetaDatadf.loc[int(year)]['ListSizePosition'])
-
+#print(QOFMetaDatadf.loc[int(year)]['ListSizePosition'])
 print("Enter the disease that you wish to get data for (AST, COPD,....")
 disease = input("Disease =")
-
 Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+filename = askopenfilename(title = "Select QOF file") # show an "Open" dialog box and return the path to the selected file
 print(filename)
 #qofDF = pd.read_excel(filename, sheet_name='AST', usecols=[2,9,12,13])
-qofDF = pd.read_excel(filename, sheet_name='AST',
-                      usecols=[2,9,QOFMetaDatadf.loc[int(year)]['ListSizePosition'],
-                               QOFMetaDatadf.loc[int(year)]['RegisterPosition']])
+qofDF = pd.read_excel(filename, sheet_name=disease,
+                      usecols=[QOFMetaDatadf.loc[int(year)]['RegionPos'],
+                               QOFMetaDatadf.loc[int(year)]['PracticeCodePos'],
+                               QOFMetaDatadf.loc[int(year)]['ListSizePos'],
+                               QOFMetaDatadf.loc[int(year)]['RegisterPos']])
 
-qofDF.columns = ['RegionName', 'PracticeCode', 'ListSize', 'Register']
+qofDF.columns = ['RegionCode', 'PracticeCode', 'ListSize', 'Register']
 #print(qofDF.head(15))
 qofDF['Year'] = year
 #qofDF['Year'] = year
-LondonQofDF = qofDF.loc[qofDF['RegionName'] == "LONDON"]
+LondonQofDF = qofDF.loc[qofDF['RegionCode'] == "Y56"]
 # returns 1330 rows
 print(LondonQofDF.head(10))
 
 #read epracur file
 Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+filename = askopenfilename(title = "Select Practices file") # show an "Open" dialog box and return the path to the selected file
 print(filename)
 
 PracticeLocations = pd.read_csv(filename)
@@ -98,7 +96,7 @@ WardsPostCodesDF.drop_duplicates(subset="Postcode", keep="first", inplace=True)
 WardsPostCodesDF.to_csv("WardsPostcode2")
 # now merge WardsDF with the LondonQofDF
 LondonQofDF = pd.merge(WardsPostCodesDF, LondonQofDF, left_on='Postcode', right_on='Postcode', how='inner')
-outPutFileName = "LondonQOF" + year
+outPutFileName = "LondonQOF-" + year
 LondonQofDF.to_csv(outPutFileName)
 
 #There may be more than 1 surgery per ward aggregate the surgeries prevalences within the ward
