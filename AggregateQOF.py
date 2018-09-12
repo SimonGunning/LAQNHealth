4a4d263854837dd6227f8d35f34f8536e5f63a86
Simon Gunning <simonmgunning@gmail.com>
2018-09-12 20:52:27 +0100

    Add files via upload

--- AggregateQOF.py
+++ AggregateQOF.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Mar  3 18:24:34 2018
-
 @author: gunning
 
 This program takes the output from the QOFProcessor. It sums the list and prevalence for
@@ -15,42 +13,30 @@ from tkinter.filedialog import askopenfilename
 #import pickle
 print("please select a QOFProcessor output file")
 Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+filename = askopenfilename(title = "Please select a QOFProcessor output file") # show an "Open" dialog box and return the path to the selected file
 LondonQOF = pd.read_csv(filename)
 print(LondonQOF.columns.values)
 print(LondonQOF["Year"][2])
 year = LondonQOF["Year"][2]
-
-#LondonQOFAggregated = pd.DataFrame(columns=['Postcode', 'Ward', 'PracticeCode', 'RegionName', 'Y1Prevalence', 'Y2Prevalence'])
-#LondonQOFAggregated1 = pd.DataFrame(columns=['SumPrev'])
-#LondonQOFAggregated2 = pd.DataFrame(columns=['SumPost'])
-
 #print(LondonQOF.head(5))
 LondonQOF.set_index('WardCode')
 LondonQOF.apply(pd.to_numeric, errors='ignore')
-
 LondonQOF[['Register','ListSize']] = LondonQOF[['Register','ListSize']].apply(pd.to_numeric, errors='coerce')
 print(LondonQOF.dtypes)
-
 #print(LondonQOF.groupby("WardCode").groups)
-grouped = LondonQOF.groupby('WardCode')
+grouped = LondonQOF.groupby(['WardCode', 'Ward'])
 
 for name,group in grouped:
-#     print(name)
-#     print(group)
-#    print(grouped["Y2Prevalance"].sum())
-#    print("type of thing" + str(type(grouped["Y1Prevalence"].sum())))
     LondonQOFAggregated1 = grouped["Register"].sum()
     LondonQOFAggregated2 = grouped["ListSize"].sum()
 
-#    print(group["PracticeCode"])
 LondonQOFAggregated1 = pd.merge(LondonQOFAggregated1.to_frame(), LondonQOFAggregated2.to_frame(), left_index=True, right_index=True, how='inner')
 #
 LondonQOFAggregated1['Prevalence_perc'] = LondonQOFAggregated1['Register']/LondonQOFAggregated1['ListSize']
 LondonQOFAggregated1['Year'] = year
 
 print(LondonQOFAggregated1.head(3))
-LondonQOFAggregatedFName = "LondonQOFAggregated-" + year
+LondonQOFAggregatedFName = "LondonQOFAggregated-" + str(year)
 LondonQOFAggregated1.to_csv(LondonQOFAggregatedFName)
 
 
--- CreateMap.py
+++ CreateMap.py
@@ -1,3 +1,4 @@
+# Combines the monitors file with the the Aggregated QOF file and maps them to a map produced from the shapefile
 import geopandas as gpd
 import pandas as pd
 from shapely.geometry import Point
@@ -5,8 +6,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 from tkinter  import Tk
 from tkinter.filedialog import askopenfilename
-print("Select the Aggregated QOF file from the AggregateQOF process")
-year = input("Year =")
+#year = input("Year =")
 Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
 filename = askopenfilename(title = "Select Monitors file") # show an "Open" dialog box and return the path to the selected file
 Monitors = pd.read_csv(filename)
@@ -26,6 +26,7 @@ wardDF = wardDF.to_crs({'init': 'epsg:4326'})
 Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
 filename = askopenfilename(title = "Select QOF file") #get QOF data
 LondonQOF = pd.read_csv(filename)
+LondonQOF['Prevalence_perc'] = LondonQOF['Prevalence_perc'] * 100
 LondonQOFDF = gpd.GeoDataFrame(LondonQOF)
 LondonQOFDF = pd.merge(wardDF, LondonQOFDF, left_on='GSS_CODE', right_on='WardCode', how='inner')
 # fill with median
@@ -42,11 +43,14 @@ newMonitors.plot(ax=base, column='NO2', cmap='YlOrRd', legend=True)
 #sm._A = []
 #cbar = plt.colorbar(sm)
 plt.title("London Ward Asthma prevalence is shown in grey. Pollution monitors are in red", fontsize=16)
-top3 = LondonQOFDF.nlargest(3,'Prevalence_perc')['WardCode']
-Top3 = "Top 3 prevalence " + str(top3)
+top3 = LondonQOFDF.nlargest(3,'Prevalence_perc')['Ward']
+Top3 = "Top 3 wards with highest disease prevalence " + str(top3)
+plt.text(0.02, 0.1, Top3, fontsize=11, transform=plt.gcf().transFigure)
+top3Monitors = newMonitors.nlargest(3,'NO2')['Ward']
+Top3Monitors = "Top 3 monitors with highest pollution levels " + str(top3Monitors)
+plt.text(0.4, 0.1, Top3Monitors, fontsize=11, color = 'red', transform=plt.gcf().transFigure)
+plt.text(0.7, 0.6, "Pollution ug/m3", fontsize=11, rotation = 'vertical', color = 'red', transform=plt.gcf().transFigure)
+plt.text(0.85, 0.6, "Disease incidence %", fontsize=11, rotation = 'vertical', color = 'black', transform=plt.gcf().transFigure)
 
-plt.text(0.02, 0.1, Top3, fontsize=14, transform=plt.gcf().transFigure)
-plt.annotate("source bla bla" ,xy=(1,1), fontsize=12, color="#555555")
 plt.axis("off")
-
 plt.show()
